e6cab47169a5e2961551bb06fd88b07a653a11d9
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-04-13 19:08:10 -0300

    created audio analyzer for ffmpeg
    basis for ffmpeg

---
+++ lecture-cut/lecture-cut.py
@@ -0,0 +1,17 @@
+import subprocess
+import pydub
+from pydub import AudioSegment
+
+def extract_audio():
+    filename = input("Enter filename: ")
+
+    command = "ffmpeg -i " + filename + " -ab 160k -ac 2 -ar 44100 -vn cut.wav"
+    subprocess.call(command, shell=True)
+
+def analyze_audo():
+    audio = AudioSegment.from_wav("cut.wav")
+    audio_chunks = pydub.silence.split_on_silence(audio, silence_thresh=-50)
+    print("Chunks generated: ", audio_chunks)
+
+extract_audio()
+analyze_audo()
