a6227d2d25bde3b46403ba203005aad59a4be0a0
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-04-24 19:27:31 -0300

    analyze_audo -> analyze_audio

--- lecture-cut/lecture-cut.py
+++ lecture-cut/lecture-cut.py
@@ -8,10 +8,13 @@ def extract_audio():
     command = "ffmpeg -i " + filename + " -ab 160k -ac 2 -ar 44100 -vn cut.wav"
     subprocess.call(command, shell=True)
 
-def analyze_audo():
+def analyze_audio():
     audio = AudioSegment.from_wav("cut.wav")
     audio_chunks = pydub.silence.split_on_silence(audio, silence_thresh=-50)
     print("Chunks generated: ", audio_chunks)
 
+def get_chunk_lengths():
+    print("default")
+
 extract_audio()
-analyze_audo()
+analyze_audio()
