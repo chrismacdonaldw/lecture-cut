47b16304d3e55873909d4a152aba4e9ab0ca6ef4
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-04-25 06:22:49 -0300

    fully working uncommented

--- lecture-cut/lecture-cut.py
+++ lecture-cut/lecture-cut.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 from pydub import AudioSegment
 from pydub.silence import detect_silence
 
@@ -7,14 +8,28 @@ def extract_audio():
 
     command = "ffmpeg -i " + filename + " -ab 160k -ac 2 -ar 44100 -vn cut.wav"
     subprocess.call(command, shell=True)
+    return filename
 
 def analyze_audio():
     audio = AudioSegment.from_wav("cut.wav")
-    return detect_silence(audio, silence_thresh = -50)
+    return detect_silence(audio, silence_thresh = -55)
 
-def get_chunk_lengths(audio_chunks):
-      for chunk in audio_chunks:
-          print(chunk)
+def generate_command(silence, filename):
+    videocmd = "ffmpeg -i " + filename + " -vf \"select='not("
+    setadd = ""
+    for i, chunk in enumerate(silence):
+        if i == 0:
+            setadd += 'between(t,%s,%s)' % ((chunk[0] + 150)/1000, (chunk[1] - 150)/1000)
+        else:
+            setadd += '+between(t,%s,%s)' % ((chunk[0] + 150)/1000, (chunk[1] - 150)/1000)
+    
+    videocmd += setadd
+    videocmd += ")',setpts=N/FRAME_RATE/TB\" -af \"aselect='not("
+    videocmd += setadd
+    videocmd += ")',asetpts=N/SR/TB\" out.mp4"
+    print(videocmd)
+    subprocess.call(videocmd, shell=True)
 
-extract_audio()
-get_chunk_lengths(analyze_audio())
+
+filename = extract_audio()
+generate_command(analyze_audio(), filename)
