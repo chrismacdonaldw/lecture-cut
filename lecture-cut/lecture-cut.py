7d88f97ba001df066ce22a4a926fbc0e713c5b18
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2019-04-25 06:54:03 -0300

    added output_file argument

--- lecture-cut/lecture-cut.py
+++ lecture-cut/lecture-cut.py
@@ -24,23 +24,28 @@ def generate_command(silence, filename):
         else:
             addcmd += '+between(t,%s,%s)' % ((chunk[0] + CUT_INTERVAL)/1000, (chunk[1] - CUT_INTERVAL)/1000)
     
-    videocmd = videocmd + '%s%s%s%s' % (
+    videocmd = videocmd + '%s%s%s%s%s' % (
         addcmd,
         COMMAND_SET_ONE,
         addcmd,
-        COMMAND_SET_TWO
+        COMMAND_SET_TWO,
+        OUTPUT_FILE
     )
     subprocess.call(videocmd, shell=True)
 
 parser = argparse.ArgumentParser(description='Trims video based on segments which are under a specified silence threshold')
+parser.add_argument('--output_file', type=str, default='out.mp4', help='name of file that will be output after trim')
 parser.add_argument('--cut_interval', type=int, default=150, help='ms of silent audio segments to be cut to allow for a custom margin of error, default is 150')
 parser.add_argument('--silence_threshold', type=int, default=-55, help='value in dBFS that when audio is lower then, will be considered silent. Default is -55')
 args = parser.parse_args()
 
+OUTPUT_FILE = args.output_file 
 CUT_INTERVAL = args.cut_interval
 SILENCE_THRESHOLD = args.silence_threshold
 COMMAND_SET_ONE = ")',setpts=N/FRAME_RATE/TB\" -af \"aselect='not("
-COMMAND_SET_TWO = ")',asetpts=N/SR/TB\" out.mp4"
+COMMAND_SET_TWO = ")',asetpts=N/SR/TB\" "
+
 
 filename = extract_audio()
 generate_command(analyze_audio(), filename)
+os.remove('cut.wav')
